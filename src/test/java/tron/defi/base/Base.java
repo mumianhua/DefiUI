06d6c1e361eb7a7fbcd837172df6cf636e6930d5
sophiawang <1975054324@qq.com>
2022-09-01 15:47:27 +0800

    modify wait function

--- src/test/java/tron/defi/base/Base.java
+++ src/test/java/tron/defi/base/Base.java
@@ -1,7 +1,6 @@
 package tron.defi.base;
 
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import tron.defi.pages.MetaMaskLoginPage;
@@ -50,20 +49,18 @@ public class Base {
       try {
         retryLoginTimes--;
         //login metaFox
-        DRIVER.manage().window().fullscreen();
-
         String metaMaskUrl = "chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/popup.html";
         DRIVER.get(metaMaskUrl);
-        DRIVER.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        waitingTime(10);
         MetaMaskLoginPage metaMaskloginPage = new MetaMaskLoginPage(DRIVER);
         metaMaskloginPage.password_input.sendKeys("Sophia123");
-        DRIVER.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        waitingTime(5);
         metaMaskloginPage.login_btn.click();
-        DRIVER.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        waitingTime(15);
 
         String first = DRIVER.getWindowHandle();
         System.out.println("first: " + first);
-        Thread.sleep(10000);
+        waitingTime(5);
 
         String to = "0xEbae50590810b05D4B403F13766f213518Edef65";
         switchMetamaskAddress(to);
@@ -72,7 +69,7 @@ public class Base {
         String tronlinkUrl = "chrome-extension://ibnejdfjmmkpcnlpebklmnkoeoihofec/packages/popup/build/index.html";
         String js = "window.open(\"" + tronlinkUrl + "\")";
         DRIVER.executeScript(js);
-        DRIVER.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        waitingTime(10);
 
         String second = DRIVER.getWindowHandle();
         System.out.println("second: "+second);
@@ -84,15 +81,15 @@ public class Base {
           System.out.println("second: "+second);
         }
         DRIVER.switchTo().window(second);
-        DRIVER.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        waitingTime(10);
 
         DRIVER.get(tronlinkUrl);
-        DRIVER.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        waitingTime(10);
         TronlinkLoginPage tlLoginPage = new TronlinkLoginPage(DRIVER);
         tlLoginPage.password_input.sendKeys("Sophia123");
-        DRIVER.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        waitingTime(5);
         tlLoginPage.login_btn.click();
-        DRIVER.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        waitingTime(10);
 
         switchTronlinkAddress("TXTNcgJHD9GPfpiTbSG2VGtfdfii9VcpEr");
         DRIVER.manage().window().maximize();
@@ -100,19 +97,18 @@ public class Base {
         //login bttc
         js = "window.open(\"" + URL + "\")";
         DRIVER.executeScript(js);
-        DRIVER.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Thread.sleep(10000);
+        waitingTime(10);
         String third = DRIVER.getWindowHandle();
         System.out.println("third: " + third);
         DRIVER.switchTo().window(second).close();
-        Thread.sleep(10000);
+        waitingTime(10);
 
         for(String tem: DRIVER.getWindowHandles()){
           third = tem;
           System.out.println("third2: " + third);
         }
         DRIVER.switchTo().window(third);
-        Thread.sleep(10000);
+        waitingTime(10);
 
       } catch (Exception e) {
         e.printStackTrace();
@@ -136,31 +132,28 @@ public class Base {
     MetaMaskMainPage metaMaskMainPage = new MetaMaskMainPage(DRIVER);
     DRIVER.manage().window().setSize(new Dimension(357, 650));
     metaMaskMainPage.address_btn.click();
-    DRIVER.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    Thread.sleep(10000);
+    waitingTime(5);
     metaMaskMainPage.search_input.sendKeys(add);
-    DRIVER.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    Thread.sleep(10000);
+    waitingTime(5);
     metaMaskMainPage.search_result.click();
-    Thread.sleep(20000);
+    waitingTime(5);
     DRIVER.manage().window().maximize();
   }
 
   public void switchTronlinkAddress(String toAddress) throws Exception{
     TronlinkMainPage tronlinkMainPage = new TronlinkMainPage(DRIVER);
-    DRIVER.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-    if (tronlinkMainPage.switch_closed_btn.isEnabled()){
-      tronlinkMainPage.switch_closed_btn.click();
-    }
+    waitingTime(5);
+//    WebElement closeBtn = DRIVER.findElementByClassName("switch-closed");
+//    if (closeBtn != null && closeBtn.isDisplayed()){
+//      tronlinkMainPage.switch_closed_btn.click();
+//    }
     tronlinkMainPage.address_btn.click();
-    DRIVER.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    waitingTime(5);
     tronlinkMainPage.search_btn.click(); //放大镜
     tronlinkMainPage.search_input.sendKeys(toAddress);
-    DRIVER.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-
+    waitingTime(5);
     tronlinkMainPage.search_result.click();
-    DRIVER.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    waitingTime(2);
   }
 
   public void waitingTime(long time) throws InterruptedException {
